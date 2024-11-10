74a3e6f8ae29752082b58131e37f7136a5e0d16f
ji233 <27987772@qq.com>
2024-11-10 11:29:53 +0800

    fix: 更正sidebar.js的241108的定向错误

--- docs/.vitepress/config/sidebar.js
+++ docs/.vitepress/config/sidebar.js
@@ -3,7 +3,7 @@ export default [
         items: [
             { text: "AY2425", link: "/events/" },
             { text: "241018", link: "/events/AY2425/241018" },
-            { text: "241101", link: "/events/AY2425/241101" },
+            { text: "241108", link: "/events/AY2425/241108" },
         ],
     },
 ];
